3b8f3cf09ff6f8de5562401be92774dcc7ebc73f
RedHoodHub <tcumpamarioisma@uss.edu.pe>
2026-07-22 22:58:30 -0500

    Eliminar archivo incorrecto

--- src/main/java/com/example/SpringHotel/repository/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringHotel.repository;
-
-import com.example.SpringHotel.entity.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
-}
